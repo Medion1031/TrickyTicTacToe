a4acb37b3be55c70508aa5edfc25dfd463bd6903
oliver.brettner <oliver.brettner@pazirik.hu>
2021-11-15 11:12:39 +0100

    Intelij setup donne

---
+++ src/game/Game.java
@@ -0,0 +1,14 @@
+package game;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Game extends JFrame implements ActionListener {
+    private static JFrame frame;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,7 @@
+import game.Game;
+
+public class main {
+    public static void main(String[] args) {
+        new Game();
+    }
+}
